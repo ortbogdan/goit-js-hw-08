e2eb015b8fd248e83b60df1242e99f61af311bda
Bogdan <ortbg1988@gmail.com>
2021-10-25 17:40:11 +0300

    [add] add task-02 solving

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
 
+
+const iframeRef = document.querySelector('iframe');
+const player = new Player(iframeRef);
+
+const localStorage = "videoplayer-current-time";
+let videoTime = {seconds: 0};
+
+player.on('timeupdate', onPlay);
+
+function onPlay(event) {
+    videoTime = event;
+    console.log(videoTime.seconds)
+}
+player.setCurrentTime(videoTime.seconds)
+// player.setCurrentTime(videoTime.seconds);
