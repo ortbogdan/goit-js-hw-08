5f5fdf4a84e5bad651119c6006658301460b9ac2
Bogdan <ortbg1988@gmail.com>
2021-10-28 09:00:49 +0300

    [fix] fix task-02 solving

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,20 @@
 import Player from '@vimeo/player';
 import throttle from "lodash.throttle"
 
-const iframeRef = document.querySelector('iframe');
-const player = new Player(iframeRef);
+const iframeRef = document.querySelector('iframe#vimeo-player');
 const LOCALSTORAGE_KEY = "videoplayer-current-time";
+const player = new Player(iframeRef);
+const getVideoTime = localStorage.getItem(LOCALSTORAGE_KEY);
 
+if (getVideoTime) {
+        player.setCurrentTime(getVideoTime)
+    }
 player.on('timeupdate', throttle(onPlay, 1000));
 
 function onPlay(event) {
     let { seconds } = event;
     localStorage.setItem(LOCALSTORAGE_KEY, seconds);
 }
-const getVideoTime = localStorage.getItem(LOCALSTORAGE_KEY);
-player.setCurrentTime(getVideoTime)
+
+    
+
